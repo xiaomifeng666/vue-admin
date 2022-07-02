6abdea71a29a1f08c7ce83ca56dff249badca81d
meifeng <1966478429@qq.com>
2022-07-02 22:39:58 +0800

    路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,6 @@ const commonRoutes = [
         component: () => import('../components/Login.vue'),
     },
     {
-        path: '/other', // 点击侧边栏跳到一个单独的路由页面，需要定义，层级和其他顶级路由一样
-        name: 'other',
-        meta: { title: '单独的路由' },
-        component: () => import('../views/Other.vue'),
-    },
-    {
         path: '/404',
         name: '404',
         meta: { title: '404' },
--- src/store/index.js
+++ src/store/index.js
@@ -15,17 +15,16 @@ const store = new Vuex.Store({
                 text: '主页', // 文本内容
             },
             {
-                name: 'other', // 要跳转的路由名称 不是路径
-                size: 18, // icon大小
-                type: 'ios-egg-outline', // icon类型
-                text: '单独的路由', // 点击侧边栏跳到一个单独的路由页面，需要提前在 router.js 定义
+                text: '二级菜单',
+                type: 'ios-paper',
             },
             {
-                size: 18, // icon大小
-                type: 'md-arrow-forward', // icon类型
-                text: '外链',
-                url: 'https://www.baidu.com',
-                isExternal: true, // 外链 跳到一个外部的 URL 页面
+                text: '二级菜单',
+                type: 'ios-paper',
+            },
+            {
+                text: '二级菜单',
+                type: 'ios-paper',
             },
             {
                 text: '二级菜单',
@@ -39,39 +38,18 @@ const store = new Vuex.Store({
                         // hidden: true,
                     },
                     {
-                        size: 18, // icon大小
-                        type: 'md-arrow-forward', // icon类型
-                        text: '外链',
-                        url: 'https://www.baidu.com',
-                        isExternal: true, // 外链 跳到一个外部的 URL 页面
+                        type: 'ios-grid',
+                        name: 't2',
+                        text: '表格',
+                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
+                        // hidden: true,
                     },
                     {
-                        text: '三级菜单',
-                        type: 'ios-paper',
-                        children: [
-                            {
-                                type: 'ios-notifications-outline',
-                                name: 'msg',
-                                text: '查看消息',
-                            },
-                            {
-                                type: 'md-lock',
-                                name: 'password',
-                                text: '修改密码',
-                            },
-                            {
-                                type: 'md-person',
-                                name: 'userinfo',
-                                text: '基本资料',
-                            },
-                            {
-                                size: 18, // icon大小
-                                type: 'md-arrow-forward', // icon类型
-                                text: '外链',
-                                url: 'https://www.baidu.com',
-                                isExternal: true, // 外链 跳到一个外部的 URL 页面
-                            },
-                        ],
+                        type: 'ios-grid',
+                        name: 't3',
+                        text: '表格',
+                        // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
+                        // hidden: true,
                     },
                 ],
             },
